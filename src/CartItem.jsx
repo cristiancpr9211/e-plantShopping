7bce705b47d33b16ffedd0c21dac30683ccf9bbc
Cristian Plata <cristianplata9211@gmail.com>
2024-11-12 08:10:01 -0500

    checkout

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -95,7 +95,7 @@ const calculateTotalAmount = () => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
